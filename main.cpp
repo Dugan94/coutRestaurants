2bd4430f2d77ffdd82c2c05724e469f4b10c14d6
Brandon Dugan <72161175+Dugan94@users.noreply.github.com>
2021-03-19 17:54:57 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string> // Required for the string class.
+using namespace std;
+
+ // Constants
+        const int TOPNUMBER = 1; // a constant that will not change throughout the life of the program
+        const char * TOPCITY = "Walnut Creek"; // a constant that will not change throughout the life of the program
+        const int TOPREVIEWS = 298; // a constant that will not change throughout the life of the program
+
+int main()
+{
+        cout << "The top 10 restaurants in Contra Costa County are: " ;
+
+       double rating ;
+
+       rating = 4.0 ;
+       cout << " (1) Bounty Hunter has a " << rating << " star rating .\n" ;
+
+        rating = 4.5 ;
+       cout << " (2) Parkside Bar & Grill has a " << rating << " star rating .\n" ;
+
+       rating = 3.5 ;
+       cout << " (3) Rooftop has a " << rating << " star rating .\n" ;
+
+       rating = 4.5 ;
+       cout << " (4) Burma 2 has a " << rating << " star rating .\n" ;
+
+       rating = 4.5 ;
+       cout << " (5) Benvenuti Ristorante has a " << rating << " star rating .\n" ;
+
+       rating = 4.0 ;
+       cout << " (6) Havana has a " << rating << " star rating .\n" ;
+
+       rating = 4.0 ;
+       cout << " (7) Danville Harvest has a " << rating << " star rating .\n" ;
+
+       rating = 4.0 ;
+       cout << " (8) Ramen Hiroshi has a " << rating << " star rating .\n" ;
+
+       rating = 4.0 ;
+       cout << " (9) Yaedam Korean BBQ has a " << rating << " star rating .\n" ;
+
+       rating = 4.0 ;
+       cout << " (10) Ohgane has a " << rating << " star rating .\n" ;
+
+       // Display message
+       cout << "The top restaurant Bounty Hunter " ;
+       cout << "is located in Walnut Creek " ;
+       cout << "and has 298 reviews." ;
+       return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
